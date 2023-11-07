59a432ed327d40b6b1e9ccd4865b72c2b7cd313e
CristhianRojas <131499371+CristhianRojasBellido@users.noreply.github.com>
2023-11-07 09:04:43 -0500

    Add files via upload

---
+++ S15_31/Main_S15_31.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package S15_31;
+
+/**
+ *
+ * @author Usuario7
+ */
+public class Main_S15_31 {
+
+    public static void main(String[] args) {
+        
+    }
+}
